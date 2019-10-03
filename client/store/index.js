dfd75a1e21be4c7e13715cd5182544ba30c6b0bd
Nevette Bailey <nevettebailey@gmail.com>
2019-10-03 14:59:37 -0400

    Feature: Redux store

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import user from './reducers/user';
+import sound from './reducers/sound';
 
-const reducer = combineReducers({ user });
+const reducer = combineReducers({ user, sound });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
