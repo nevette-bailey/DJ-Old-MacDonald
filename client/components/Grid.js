aabe1f216ac224703bb9d228e339e8f0af579b7e
Lyndsey Reese <lmariereese@gmail.com>
2019-10-04 20:32:28 -0400

    Feature: play button starts playing a sequence of sounds and pause stops it

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import InstrumentRow from './InstrumentRow';
-import { resetSoundThunk } from '../store/reducers/sounds';
+import { resetSoundThunk, getSoundThunk } from '../store/reducers/sounds';
+const Tone = require('Tone');
 
 class Grid extends React.Component {
   constructor() {
     super();
-    this.state = { isToggleOn: true };
+    this.state = {
+      isToggleOn: true,
+      currentSequence: {}
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.playSounds = this.playSounds.bind(this);
+    this.createNotes = this.createNotes.bind(this);
+    // this.handleReset = this.handleReset.bind(this);
   }
 
+  // Accepts an array of true/false values and a note (e.g. 'C4')
+  // and returns an array containing note/null
+  // where null represents a rest and a note is a beat
+  createNotes(soundState, note) {
+    let notes = [];
+    for (let i = 0; i < soundState.length; i++) {
+      if (soundState[i]) {
+        notes.push(note);
+      } else {
+        notes.push(null);
+      }
+    }
+    return notes;
+  }
+
+  playSounds() {
+    const { sound1 } = this.props.sounds;
+    let notes = this.createNotes(sound1, 'C4');
+    const synth = new Tone.MembraneSynth().toMaster();
+
+    const synthPart = new Tone.Sequence(
+      function(time, note) {
+        synth.triggerAttackRelease(note, '10hz', time);
+      },
+      notes,
+      '8n'
+    );
+    synthPart.start();
+    Tone.Transport.start();
+    console.log(synthPart);
+  }
+
+  // handleReset() {
+  //   Tone.Transport.clear();
+  // }
+
   handleClick = () => {
+    if (this.state.isToggleOn) {
+      this.playSounds();
+    } else {
+      Tone.Transport.stop();
+    }
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
   };
 
   render() {
+    // console.log('STATE: ', this.props.sounds);
     return (
       <div className="wrapper">
         <InstrumentRow />
@@ -25,6 +74,7 @@ class Grid extends React.Component {
         </button>
         <button
           type="submit"
+          // onClick={this.handleReset}
           onClick={() => this.props.resetSoundThunk()}
           className="button"
         >
@@ -35,10 +85,17 @@ class Grid extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    // sounds is { sound1: [Array 16]}
+    sounds: state.sounds
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     resetSoundThunk: () => dispatch(resetSoundThunk())
   };
 };
 
-export default connect(null, mapDispatchToProps)(Grid);
+export default connect(mapStateToProps, mapDispatchToProps)(Grid);
--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -4,6 +4,7 @@
 export const GET_USER = 'GET_USER';
 export const REMOVE_USER = 'REMOVE_USER';
 
+export const GET_SOUND = 'GET_SOUND';
 export const UPDATE_SOUND = 'UPDATE_SOUND';
 export const UPDATE_SOUND_ERROR = 'UPDATE_SOUND_ERROR';
 export const RESET_SOUND = 'RESET_SOUND';
