0646daff6ddb98eaac69d42529263060247f891d
Nevette Bailey <nevettebailey@gmail.com>
2019-10-07 12:12:31 -0400

    Feature: Tone Player - sound starts playing clip

--- client/components/InstrumentRow.js
+++ client/components/InstrumentRow.js
@@ -4,34 +4,45 @@ import { connect } from 'react-redux';
 const Tone = require('Tone');
 
 class InstrumentRow extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      sound1: new Tone.Player({
+        url: 'https://actions.google.com/sounds/v1/animals/dog_barking.ogg',
+        autostart: true,
+        time: '1'
+        // loop: true,
+        // loopEnd: 1,
+        // mute: false
+      }).toMaster(),
+      sound2: new Tone.MembraneSynth().toMaster()
+    };
+  }
   // toggleSwitch = event => {
   //
   // }
-  // };
-  // const synth = new Tone.Player('https://actions.google.com/sounds/v1/animals/animal_squealing.ogg', function(){}).toMaster();
-  // makeSound() {
-  //   this.synth.start();
-  // }
 
-  // stopSound() {
-  //   this.synth.stop()
+  // componentDidMount() {
+  //   this.setState({ sound1: { mute: true } });
+  //   this.state.sound1.start();
+  //   this.state.sound1.stop();
   // }
 
   onClickFunction(soundId, idx) {
-<<<<<<< HEAD
-    this.props.updateSound(soundId, idx);
+    // update boolean value of box in redux state
 
-    const synth = new Tone.Player({
-      url: 'https://actions.google.com/sounds/v1/animals/animal_squealing.ogg',
-      autostart: true,
-      loopEnd: 1
-    }).toMaster();
-    if (!this.props.sound1[idx]) synth.start();
-    if (this.props.sound1[idx]) synth.stop();
-=======
+    // check whether sound should be triggered
+    if (!this.props.sound1[idx] && this.state.sound1.loaded) {
+      //play imported sound when box is clicked
+      this.state.sound1.start(0);
+      this.state.sound1.stop('2n');
+
+      //create a sound when box is clicked
+      // this.synth.triggerAttackRelease('C4', '4n');
+    } else {
+      this.state.sound1.stop();
+    }
     this.props.updateSoundThunk(soundId, idx);
-    if (!this.props.sound1[idx]) this.makeSound();
->>>>>>> 7936f7f7f127ef9a7e042142400efb9a4f9170dc
   }
 
   render() {
@@ -44,7 +55,6 @@ class InstrumentRow extends React.Component {
               data-index={idx}
               key={idx}
               value={elem}
-              // onClick={() => this.props.updateSoundThunk('sound1', idx)}
               onClick={() => this.onClickFunction('sound1', idx)}
             />
           );
