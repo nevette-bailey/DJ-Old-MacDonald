8b58f2973a59f4cf804f2df11243ec60179ac1e4
bicipeng <bicipeng@gmail.com>
2019-10-07 14:37:54 -0400

    feat: added post API for loops

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const { User, Loop } = require('../db/models');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -9,9 +9,31 @@ router.get('/', async (req, res, next) => {
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email']
-    })
-    res.json(users)
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+//post a track to specific user
+router.post('/loops', async (req, res, next) => {
+  try {
+    //association will generate a user_loop tabld
+
+    const loop = await Loop.findOrCreate({
+      //create new loop in our loop model
+      where: {
+        title: req.body.title,
+        sound1: req.body.sound1 // sound1 for now. SoundId later?
+      }
+    });
+
+    //userId was created autommatically due to association
+
+    const currentUser = await User.findByPk(req.body.userId);
+    await currentUser.addLoop(loop[0]);
+    res.json(loop[0]);
+  } catch (err) {
+    next(err);
+  }
+});
