37512f41c7b91c29efae261c27a1d80cd8f8223f
Nevette Bailey <nevettebailey@gmail.com>
2019-10-07 22:01:39 -0400

    Feature: external audio API functions on click

--- client/components/InstrumentRow.js
+++ client/components/InstrumentRow.js
@@ -27,14 +27,8 @@ class InstrumentRow extends React.Component {
     // check whether sound should be triggered
     if (!this.props.sound1[idx] && this.state.sound1.loaded) {
       //play imported sound when box is clicked
-      this.state.sound1.restart();
+      this.state.sound1.restart(undefined, undefined, 0.6);
       this.state.sound2.triggerAttackRelease('C4', '8n');
-      Tone.Transport.start();
-
-      //stops playing the looping clip of the imported sound
-      this.state.sound1.stop(0.6);
-    } else if (Tone.Transport.state === 'started') {
-      Tone.Transport.stop();
     }
     // update boolean value of box in redux state
     this.props.updateSoundThunk(soundId, idx);
