697d7a6cd80bba086d6dc9f44b07079602c90187
Nevette Bailey <nevettebailey@gmail.com>
2019-10-07 20:10:55 -0400

    Feature: plays bark on click, less bugs

--- client/components/InstrumentRow.js
+++ client/components/InstrumentRow.js
@@ -28,17 +28,13 @@ class InstrumentRow extends React.Component {
     if (!this.props.sound1[idx] && this.state.sound1.loaded) {
       //play imported sound when box is clicked
       this.state.sound1.restart();
-      // this.state.sound2.triggerAttackRelease('C4', '8n');
+      this.state.sound2.triggerAttackRelease('C4', '8n');
       Tone.Transport.start();
 
       //stops playing the looping clip of the imported sound
-      // setTimeout(function () {this.state.sound1.stop()}, 600)
       this.state.sound1.stop(0.6);
-    } else {
-      this.state.sound1.stop();
-      if (Tone.Transport.state === 'started') {
-        Tone.Transport.stop();
-      }
+    } else if (Tone.Transport.state === 'started') {
+      Tone.Transport.stop();
     }
     // update boolean value of box in redux state
     this.props.updateSoundThunk(soundId, idx);
