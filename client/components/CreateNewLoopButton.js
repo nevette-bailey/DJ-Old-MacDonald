d1703476aac2da907897d20e12e595125bbf8c77
bicipeng <bicipeng@gmail.com>
2019-10-09 17:45:03 -0400

    feat: added pop up for create new loop button

--- client/components/CreateNewLoopButton.js
+++ client/components/CreateNewLoopButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { createNewLoopThunk } from '../store/reducers/loops';
 import { connect } from 'react-redux';
-
+import Popup from 'reactjs-popup';
 class CreateNewLoopButton extends Component {
   constructor(props) {
     super(props);
@@ -10,22 +10,49 @@ class CreateNewLoopButton extends Component {
   handleSubmit(event) {
     event.preventDefault();
     this.props.createNewLoopThunk();
+    // onClick={this.handleSubmit}
+    close();
   }
   render() {
     return (
       <div>
-        <button className="button" type="submit" onClick={this.handleSubmit}>
-          CREATE NEW
-        </button>
+        <Popup
+          trigger={
+            <button className="button" type="submit">
+              CREATE NEW
+            </button>
+          }
+          modal
+        >
+          {close => (
+            <div className="modal">
+              <a className="close" onClick={close}>
+                &times;
+              </a>
+
+              <div className="content">
+                Do you want to save the current loop?
+              </div>
+              <div className="actions">
+                <button className="button" type="button">
+                  Yes
+                </button>
+
+                <button
+                  className="button"
+                  onClick={this.handleSubmit}
+                  type="submit"
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          )}
+        </Popup>
       </div>
     );
   }
 }
-// const mapStateToProps = state => {
-//     return {
-
-//     };
-//   };
 
 const mapDispatchToProps = dispatch => {
   return {
