741cbaa5d9e06d2354aeac4b7e225b38f16cf20f
Lyndsey Reese <lmariereese@gmail.com>
2019-10-11 16:19:51 -0400

    Style: Reorganized CSS file

--- client/store/reducers/user.js
+++ client/store/reducers/user.js
@@ -35,6 +35,7 @@ export const auth = (email, password, method) => async dispatch => {
 
   try {
     dispatch(getUser(res.data));
+    // history.push('/grid');
     // history.push('/home');
   } catch (dispatchOrHistoryErr) {
     console.error(dispatchOrHistoryErr);
