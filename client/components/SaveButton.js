e565c9956ef13c06138c14d9e9afe598ca932553
Lyndsey Reese <lmariereese@gmail.com>
2019-10-17 13:52:24 -0400

    Feature: AuthPopup and LoopsInfoPopup overlay grid

--- client/components/SaveButton.js
+++ client/components/SaveButton.js
@@ -3,14 +3,34 @@ import { saveLoopThunk } from '../store/reducers/loops';
 import { connect } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 import { withRouter } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 import { toast } from 'react-toastify';
+import LoopsInfoPopup from './LoopsInfoPopup';
+import AuthPopup from './AuthPopup';
 
 class SaveButton extends Component {
   constructor(props) {
     super(props);
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.disabledSaveButton = this.disabledSaveButton.bind(this);
+    this.saveExisting = this.saveExisting.bind(this);
   }
+
+  //if user is logged in & saving an existing loop, forward the sound and id, title and description in null.
+  saveExisting(event) {
+    event.preventDefault();
+    this.props.saveLoopThunk(this.props.sounds, this.props.loopId);
+    toast('Loop Saved!', {
+      position: 'bottom-right',
+      autoClose: 2000
+    });
+  }
+
+  // // if user logged in & saving a new loop
+  // saveNew() {
+
+  // }
 
   handleSubmit(event) {
     event.preventDefault();
@@ -29,17 +49,12 @@ class SaveButton extends Component {
       }
     } else {
       //if not looged in and changes are made to the loops.
-      console.log('historyyyyy', this.props.history);
       this.props.history.push('authpopup');
     }
   }
 
-  showPopup() {}
-
-  render() {
-    console.log('loopId :', this.props.loopId);
-    console.log('isSaved :', this.props.isSaved);
-    return this.props.isSaved ? (
+  disabledSaveButton() {
+    return (
       <div className="icontext">
         <button
           type="submit"
@@ -54,13 +69,57 @@ class SaveButton extends Component {
         </button>
         Save
       </div>
-    ) : (
+    );
+  }
+
+  render() {
+    console.log('loopId :', this.props.loopId);
+    console.log('isSaved :', this.props.isSaved);
+    return this.props.isSaved ? (
+      this.disabledSaveButton()
+    ) : this.props.loopId ? (
       <div className="icontext">
-        <button type="submit" onClick={this.handleSubmit} className="button">
+        <button
+          type="submit"
+          onClick={event => this.saveExisting(event)}
+          className="button"
+        >
           <img src="https://img.icons8.com/material-rounded/28/000000/save.png" />
         </button>
         Save
       </div>
+    ) : this.props.user.id ? (
+      <Popup
+        trigger={
+          <div className="icontext">
+            <button
+              type="submit"
+              // onClick={this.handleSubmit}
+              className="button"
+            >
+              <img src="https://img.icons8.com/material-rounded/28/000000/save.png" />
+            </button>
+            Save
+          </div>
+        }
+        modal
+      >
+        {close => <LoopsInfoPopup close={close} />}
+      </Popup>
+    ) : (
+      <Popup
+        trigger={
+          <div className="icontext">
+            <button type="submit" className="button">
+              <img src="https://img.icons8.com/material-rounded/28/000000/save.png" />
+            </button>
+            Save
+          </div>
+        }
+        modal
+      >
+        {close => <AuthPopup close={close} />}
+      </Popup>
     );
   }
 }
@@ -79,6 +138,7 @@ const mapDispatchToProps = dispatch => {
     saveLoopThunk: (newLoop, id) => dispatch(saveLoopThunk(newLoop, id))
   };
 };
+
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(SaveButton)
 );
